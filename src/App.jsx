9f5c3758341238d5b699bdab69140d7233a8f5eb
Victor <58487146+c-n-o-t-e@users.noreply.github.com>
2025-12-05 07:57:50 +0100

    Fix typo in scrolling description

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more about Vite and React 🙂 blazing fast scrolling 📜.
+        Click the Vite and React logos to learn more about Vite and React 🙂 blazing-fast scrolling 📜.
       </p>
     </>
   )
